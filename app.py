84ec6df0db8f3d7ebc60b85b14053966b579b015
Nan Huang <nhuang@uwaterloo.ca>
2017-04-30 23:57:11 -0400

    Remove local database config

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/todo-anywhere'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/todo-anywhere'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
